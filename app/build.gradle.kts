2790f330598ead3aca49a749541dff7b5c30d61e
serhii93git <s.g.demchenko93@gmail.com>
2026-04-08 10:41:44 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,8 @@ dependencies {
     kapt(libs.androidx.room.compiler)
 
     implementation(libs.kotlinx.coroutines.android)
-        implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.androidx.datastore.preferences)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
